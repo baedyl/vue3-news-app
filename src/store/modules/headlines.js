7867a202efafcb23228f0aeb7dc3ef4f91772b18
emmanuel <emmanuel@kaokeb.com>
2021-07-25 14:28:02 +0100

    Added search action and history in headlines state module

--- src/store/modules/headlines.js
+++ src/store/modules/headlines.js
@@ -1,15 +1,29 @@
+// API queries
 import news from "../../api/news";
 
 // INITIAL STATE
 const state = () => ({
   news: [],
+  history: [],
   articleDetails: null,
+  loading: false,
+  historyLength: 5
 });
 
 // GETTERS
 const getters = {
-  allHeadlines: state => { return state.news },
-  articleDetails: state => { return state.articleDetails },
+  allHeadlines: (state) => {
+    return state.news;
+  },
+  articleDetails: (state) => {
+    return state.articleDetails;
+  },
+  loadingState: (state) => {
+    return state.loading;
+  },
+  historyHeadlines: (state) => {
+    return state.history;
+  },
 };
 
 // MUTATIONS
@@ -17,37 +31,60 @@ const mutations = {
   setNews(state, news) {
     // Fills in the headlines with a human readable date format
     state.news = news.map((article) => {
-        const options = { year: "numeric", month: "long", day: "numeric" }
-        article.publishedAt = new Date(article.publishedAt).toUTCString(undefined, options)
-        return article
+      const options = { year: "numeric", month: "long", day: "numeric" };
+      article.publishedAt = new Date(article.publishedAt).toUTCString(
+        undefined,
+        options
+      );
+      return article;
     });
+    state.loading = false;
   },
-  setHeadlineDetails(state, details) {
+  setHeadlineDetails(state, params) {
+    const { details, log } = { ...params }
     // Fills in the current headline
-    state.articleDetails = details
+    state.articleDetails = details;
+    if (log) {
+    // Fills in the history of headlines
+    const historyLength = state.history.length;
+    if (historyLength >= state.historyLength) {
+      state.history.shift();
+    }
+      state.history.push(details);
+    }
   },
   setHeadlineTitle(state, params) {
     // Updates a headline's title
-    const { title, index } = { ...params }
-    state.news[index].title = title
+    const { title, index } = { ...params };
+    state.news[index].title = title;
+  },
+  setLoading(state, value) {
+    // Fills in the loading state
+    state.loading = value;
   },
 };
 
 // ACTIONS
 const actions = {
   async getNewsData({ commit }, source) {
-    console.log(source);
+    commit("setLoading", true);
     const newsData = source ? await news.getNews(source) : await news.getNews();
     commit("setNews", newsData);
   },
-  async getCurrentHeadline({ state, commit, dispatch }, title) {
+  async searchNewsData({ commit }, searchText) {
+    commit("setLoading", true);
+    const newsData = await news.searchNews(searchText);
+    commit("setNews", newsData);
+  },
+  async getCurrentHeadline({ state, commit, dispatch }, params) {
+    const { title, log } = { ...params }
     if (state.news.length < 1) {
       // In case the news array is empty,
       // Let's fetch the data
-      await dispatch('getNewsData')
+      await dispatch("getNewsData");
     }
-    const article =  state.news.filter( article => article.title === title)[0];
-    commit("setHeadlineDetails", article);
+    const article = state.news.filter((article) => article.title === title)[0];
+    commit("setHeadlineDetails", { details: article, log });
   },
   updateHeadlineTitle({ state, commit }, params) {
     for (let index = 0; index < state.news.length; index++) {
@@ -56,7 +93,7 @@ const actions = {
         // Change the headline title
         commit("setHeadlineTitle", {
           title: params.newTitle,
-          index
+          index,
         });
         break;
       }
